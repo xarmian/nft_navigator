9bf282f276f205f37e885b50012afed78f719fb6
xarmian <xarmian@gmail.com>
2024-03-11 23:38:35 -0400

    mobile ui collection tweaks

--- src/routes/(site)/collection/[cid]/+page.svelte
+++ src/routes/(site)/collection/[cid]/+page.svelte
@@ -102,8 +102,8 @@
     <img src="{data.collection?.highforgeData?.coverImageURL ?? tokens[0].metadata.image}" class="banner_img object-cover" />
     <img src="{data.collection?.highforgeData?.coverImageURL ?? tokens[0].metadata.image}" class="banner_img2 w-1/2 object-cover" />
     <div class="mask_dark flex justify-center h-full absolute w-full content-center bg-slate-100 dark:bg-slate-800">
-        <div class="collection_detail flex justify-center content-center space-x-10">
-            <div class="flex h-fullplace-items-center space-between flex-col space-y-1">
+        <div class="collection_detail w-1/2 flex justify-center content-center md:space-x-10 pr-2">
+            <div class="flex h-fullplace-items-center space-between flex-col space-y-1 w-3/4 md:w-auto">
                 {#if data.collection?.highforgeData}
                     <div class="p-4 overflow-auto mb-auto">
                         <div class="text-4xl font-bold">{data.collection?.highforgeData?.title??collectionName}</div>
@@ -126,7 +126,7 @@
                         </div>
                         <div>
                             <div class="text-sm">Creator</div>
-                            <div class="text-lg text-blue-300"><a href='https://voi.observer/explorer/account/{collection?.creator}' target="_blank">{collection?.creator.substring(0,8)}...</a></div>
+                            <div class="text-lg text-blue-300 hover:text-blue-200"><a href='https://voi.observer/explorer/account/{collection?.creator}' target="_blank">{collection?.creator.substring(0,8)}...</a></div>
                         </div>
                     </div>
                 </div>
@@ -140,63 +140,47 @@
     </div>
 </div>
 <div class="flex pb-16">
-    {#if !isMobile && displayTab == 'tokens'}
-        <div class="p-4">
-            <div class="relative self-start">
-                <input type="text" placeholder="Search" bind:value={searchText} bind:this={inputElement} class="p-2 border border-gray-300 rounded-lg dark:bg-gray-600 w-full pr-10"/>
-                {#if searchText}
-                    <button class="absolute inset-y-0 right-0 pr-3 flex items-center w-10" on:click={() => { searchText = ''; inputElement.focus(); }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5 text-gray-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                        </svg>
-                    </button>
-                {/if}
-            </div>
-            {#each Object.entries(categories) as [category, traits]}
-                <div class="mt-2 mb-2">
-                    <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" for={category}>
-                        {category}
-                    </label>
-                    <div class="relative">
-                        <select bind:value={filters[category]} class="block appearance-none w-48 bg-white border border-gray-300 dark:border-gray-500 text-gray-700 dark:bg-gray-600 dark:text-gray-200 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id={category}>
-                            <option value="">All</option>
-                            {#each Object.entries(traits) as [trait, count]}
-                                <option value={trait}>{trait+' ('+count+')'}</option>
-                            {/each}
-                        </select>
-                    </div>
-                </div>
-            {/each}
-        </div>
-    {/if}
-    <div class="w-full">
-        {#if isMobile}
-            <div class="flex justify-center">
-                <button on:click={() => window.open('https://highforge.io/project/'+contractId)} class="bg-opacity-20 m-4 px-4 py-1 bg-blue-600 text-gray-500 dark:text-gray-100 rounded-md transition-colors duration-200 ease-in-out hover:bg-blue-900 outline-1">
-                    High Forge <i class="fas fa-external-link-alt"></i>
+    <div class="p-4 hidden md:block">
+        <div class="relative self-start">
+            <input type="text" placeholder="Search" bind:value={searchText} bind:this={inputElement} class="p-2 border border-gray-300 rounded-lg dark:bg-gray-600 w-full pr-10"/>
+            {#if searchText}
+                <button class="absolute inset-y-0 right-0 pr-3 flex items-center w-10" on:click={() => { searchText = ''; inputElement.focus(); }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5 text-gray-500">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
                 </button>
-                {#if data.collection?.gameData}
-                    <button on:click={() => window.open('https://nft-games.boeieruurd.com/collections/'+contractId)} class="bg-opacity-20 m-4 px-4 py-1 bg-blue-600 text-white rounded-md transition-colors duration-200 ease-in-out hover:bg-blue-900">
-                        <img src={voiGamesImage} class="h-12 inline-block" alt="Voi Games" /><i class="fas fa-external-link-alt inline-block"></i>
-                    </button>
-                {/if}
+            {/if}
+        </div>
+        {#each Object.entries(categories) as [category, traits]}
+            <div class="mt-2 mb-2">
+                <label class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" for={category}>
+                    {category}
+                </label>
+                <div class="relative">
+                    <select bind:value={filters[category]} class="block appearance-none w-48 bg-white border border-gray-300 dark:border-gray-500 text-gray-700 dark:bg-gray-600 dark:text-gray-200 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id={category}>
+                        <option value="">All</option>
+                        {#each Object.entries(traits) as [trait, count]}
+                            <option value={trait}>{trait+' ('+count+')'}</option>
+                        {/each}
+                    </select>
+                </div>
             </div>
-        {/if}
-        <div class="flex {isMobile ? 'justify-center' : 'justify-end'}">
-            <div class="p-4 flex flex-row">
+        {/each}
+    </div>
+    <div class="w-full">
+        <div class="flex justify-center md:justify-end">
+            <div class="pt-2 md:pt-4 md:p-4 flex flex-row space-x-1">
                 {#if displayTab == 'tokens'}
+                    <div class="flex pl-4 justify-center h-10 md:hidden">
+                        <input type="text" placeholder="Search" bind:value={searchText} class="border border-gray-300 rounded-lg dark:bg-gray-600" />
+                    </div>
                     <Switch label="For Sale" bind:checked={forSaleCollection} ></Switch>
                 {/if}
                 <Select bind:value={displayTab} options={[{id: 'tokens',name:'Tokens'},{id:'sales',name:'Sales'}]}></Select>
             </div>
         </div>
         {#if displayTab == 'tokens'}
-            {#if isMobile}
-                <div class="flex pl-4 justify-center">
-                    <input type="text" placeholder="Search" bind:value={searchText} class="p-2 border border-gray-300 rounded-lg dark:bg-gray-600" />
-                </div>
-            {/if}
-            <div class="flex flex-wrap flex-grow {isMobile ? 'justify-center' : 'justify-start'}">
+            <div class="flex flex-wrap flex-grow justify-center md:justify-start'}">
                 {#each filteredTokens.slice(0, displayCount) as token (token.tokenId)}
                     <div class="p-1">
                         <TokenCard {token} />
