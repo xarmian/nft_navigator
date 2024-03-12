2c8ef51a7dc01e1277c2b4f33ee672d73ac16a44
xarmian <xarmian@gmail.com>
2024-03-12 12:23:09 -0400

    better fix for for sale page due to large number of listings

--- src/routes/(site)/forsale/+page.ts
+++ src/routes/(site)/forsale/+page.ts
@@ -5,18 +5,27 @@ import voiGames from '$lib/data/voiGames.json';
 import algosdk from 'algosdk';
 
 export const load = (async ({ fetch }) => {
-    const data = await (await fetch('https://arc72-idx.nftnavigator.xyz/nft-indexer/v1/mp/listings?active=true&limit=500')).json();
+    const data = await (await fetch('https://arc72-idx.nftnavigator.xyz/nft-indexer/v1/mp/listings?active=true')).json();
     const listings = data.listings as Listing[];
     const tokens: Token[] = [];
 
-    console.log(listings.length);
-
     // build array of collectionId+'_'+tokenId from listings and use with tokenIds[] to get token data
-    const tokenIds = listings.map((l: Listing) => l.collectionId + '_' + l.tokenId).join(',');
+    const tokenIds = listings.map((l: Listing) => l.collectionId + '_' + l.tokenId);
 
-    const tokenData = await (await fetch(`https://arc72-idx.nftnavigator.xyz/nft-indexer/v1/tokens/?tokenIds=${tokenIds}`)).json();
-    //const tokenData = { tokens: [] };
+    // Split tokenIds into chunks of 500
+    const chunks = [];
+    for (let i = 0; i < tokenIds.length; i += 500) {
+        chunks.push(tokenIds.slice(i, i + 500));
+    }
 
+    // Make a request for each chunk and concatenate the results
+    const tokenData = { tokens: <Token[]>[] };
+    for (const chunk of chunks) {
+        const response = await fetch(`https://arc72-idx.nftnavigator.xyz/nft-indexer/v1/tokens/?tokenIds=${chunk.join(',')}`);
+        const data = await response.json();
+        tokenData.tokens = [...tokenData.tokens, ...data.tokens];
+    }
+    
     tokenData.tokens.forEach((data: any) => {
         const metadata = JSON.parse(data.metadata);
         const listing = listings?.find((l: Listing) => l.collectionId === data.contractId && l.tokenId === data.tokenId);
