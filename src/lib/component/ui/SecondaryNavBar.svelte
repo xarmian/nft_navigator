9ea9472b1315fc07e5ddcefe23e1dd93bcff0396
xarmian <xarmian@gmail.com>
2024-05-31 15:49:37 -0400

    secondary nav bar

--- src/lib/component/ui/SecondaryNavBar.svelte
+++ src/lib/component/ui/SecondaryNavBar.svelte
@@ -1,11 +1,11 @@
-<div class="bg-white dark:bg-gray-800 shadow-md text-sm">
+<div class="bg-white dark:bg-gray-900 shadow-md text-sm">
     <div class="container mx-auto px-4 py-2">
-        <ul class="flex space-x-8 place-content-end">
+        <ul class="flex space-x-8 place-content-end items-center">
             <li><a href="/blog/arc72" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">What are ARC-72 NFTs?</a></li>
             <li><a href="https://arc72-idx.nftnavigator.xyz/api-docs" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">Voi ARC-72 Indexer</a> <i class="fas fa-external-link"></i></li>
             <li><a href="https://voi.network" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">What is Voi?</a> <i class="fas fa-external-link"></i></li>
             <li><a href="https://voirewards.com/phase2" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">More Quests</a> <i class="fas fa-external-link"></i></li>
-            <li><a href="https://x.com/voinftnavigator" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline"><i class="fa-brands fa-x"></i></a></li>
+            <li><a href="https://x.com/voinftnavigator" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline"><i class="fa-brands fa-twitter"></i></a></li>
         </ul>
     </div>
 </div>
