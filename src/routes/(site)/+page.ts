e47dc0104f5e39c4e7b50c439da34b0415d6d352
xarmian <xarmian@gmail.com>
2024-08-06 23:00:13 -0400

    reduce sales count for home page for performance

--- src/routes/(site)/+page.ts
+++ src/routes/(site)/+page.ts
@@ -13,7 +13,7 @@ export const load = (async ({ fetch }) => {
 
     let collections: Collection[] = await getCollections({ fetch, includes: 'unique-owners', contractId: undefined });
 
-    const sales = await getSales({ contractId: undefined, sortBy: '-round', limit: 500, fetch: fetch });
+    const sales = await getSales({ contractId: undefined, sortBy: '-round', limit: 200, fetch: fetch });
 
     if (sales) {
         const popularity = sales.reduce((acc: any, sale: any) => {
