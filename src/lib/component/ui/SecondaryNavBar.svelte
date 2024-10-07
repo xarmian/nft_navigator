ac6bc4175e2de3c9383c5d651ff022e9001d76d8
xarmian <xarmian@gmail.com>
2024-10-07 15:21:35 -0400

    x icon, avm-wallet patch

--- src/lib/component/ui/SecondaryNavBar.svelte
+++ src/lib/component/ui/SecondaryNavBar.svelte
@@ -5,7 +5,7 @@
             <li><a href="/blog/arc72" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">What are ARC-72 NFTs?</a></li>
             <!--<li><a href="https://arc72-idx.nftnavigator.xyz/api-docs" target="_blank" class="hiddentext-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">Voi ARC-72 Indexer</a> <i class="fas fa-external-link"></i></li>-->
             <!--<li><a href="https://voirewards.com/quests" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">More Quests</a> <i class="fas fa-external-link"></i></li>-->
-            <li><a href="https://x.com/voinftnavigator" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline">X/Twitter <i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href="https://x.com/voinftnavigator" target="_blank" class="text-gray-700 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline"><i class="fab fa-x-twitter"></i>/Twitter</a></li>
         </ul>
     </div>
 </div>
